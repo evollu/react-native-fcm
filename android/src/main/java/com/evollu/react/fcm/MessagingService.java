62dce36879de38157fb7091a57ef7534476343b9
sondremare <sondremo@gmail.com>
2016-12-01 17:39:48 +0100

    feature(android): add support for badge count (#207)
    * feature(android): add support for badge count (#1)
    
    Using Leolin's ShortcutBadger we are able to support any badge counts that are sent as part of the notifications data-payload.
    
    * refactor(android): expand badge support (#3)

--- android/src/main/java/com/evollu/react/fcm/MessagingService.java
+++ android/src/main/java/com/evollu/react/fcm/MessagingService.java
@@ -1,7 +1,9 @@
 package com.evollu.react.fcm;
 
+import java.util.Map;
 import android.content.Intent;
 import android.util.Log;
+import me.leolin.shortcutbadger.ShortcutBadger;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -16,6 +18,26 @@ public class MessagingService extends FirebaseMessagingService {
         Log.d(TAG, "Remote message received");
         Intent i = new Intent("com.evollu.react.fcm.ReceiveNotification");
         i.putExtra("data", remoteMessage);
+        handleBadge(remoteMessage);
         sendOrderedBroadcast(i, null);
+    }
+
+    public void handleBadge(RemoteMessage remoteMessage) {
+        BadgeHelper badgeHelper = new BadgeHelper(this);
+        if (remoteMessage.getData() == null) {
+            return;
+        }
+
+        Map data = remoteMessage.getData();
+        if (data.get("badge") == null) {
+            return;
+        }
+
+        try {
+            int badgeCount = Integer.parseInt((String)data.get("badge"));
+            badgeHelper.setBadgeCount(badgeCount);
+        } catch (Exception e) {
+            Log.e(TAG, "Badge count needs to be an integer", e);
+        }
     }
 }
