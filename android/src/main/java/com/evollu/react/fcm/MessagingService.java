d60b36d5e65573dce6ac5a0374e36d2c8feca119
Libin Lu <llu@evolenthealth.com>
2016-12-05 13:09:25 -0500

    custom_notification

--- android/src/main/java/com/evollu/react/fcm/MessagingService.java
+++ android/src/main/java/com/evollu/react/fcm/MessagingService.java
@@ -2,11 +2,13 @@ package com.evollu.react.fcm;
 
 import java.util.Map;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
-import me.leolin.shortcutbadger.ShortcutBadger;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MessagingService extends FirebaseMessagingService {
@@ -19,6 +21,7 @@ public class MessagingService extends FirebaseMessagingService {
         Intent i = new Intent("com.evollu.react.fcm.ReceiveNotification");
         i.putExtra("data", remoteMessage);
         handleBadge(remoteMessage);
+        buildLocalNotification(remoteMessage);
         sendOrderedBroadcast(i, null);
     }
 
@@ -38,6 +41,24 @@ public class MessagingService extends FirebaseMessagingService {
             badgeHelper.setBadgeCount(badgeCount);
         } catch (Exception e) {
             Log.e(TAG, "Badge count needs to be an integer", e);
+        }
+    }
+
+    public void buildLocalNotification(RemoteMessage remoteMessage) {
+        if(remoteMessage.getData() == null){
+            return;
+        }
+        Map<String, String> data = remoteMessage.getData();
+        String customNotification = data.get("custom_notification");
+        if(customNotification != null){
+            try {
+                Bundle bundle = BundleJSONConverter.convertToBundle(new JSONObject(customNotification));
+                FIRLocalMessagingHelper helper = new FIRLocalMessagingHelper(this.getApplication());
+                helper.sendNotification(bundle);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
