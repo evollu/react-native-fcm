d799b1bc623aa54d6a89e5bb3723a697c8a825f3
Libin Lu <llu@evolenthealth.com>
2017-01-06 16:36:07 -0500

    update example for 4.0.0

--- Examples/simple-fcm-client/ios/SimpleFcmClient/AppDelegate.m
+++ Examples/simple-fcm-client/ios/SimpleFcmClient/AppDelegate.m
@@ -9,7 +9,7 @@
 
 #import "AppDelegate.h"
 
-#import "RCTBundleURLProvider.h"
+#import <React/RCTBundleURLProvider.h>
 #import "RCTRootView.h"
 
 #import "RNFIRMessaging.h"
@@ -35,34 +35,31 @@
   [self.window makeKeyAndVisible];
   
   [FIRApp configure];
-  #if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
   [[UNUserNotificationCenter currentNotificationCenter] setDelegate:self];
-  #endif
   
   return YES;
 }
 
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
+{
   [[NSNotificationCenter defaultCenter] postNotificationName:FCMNotificationReceived object:self userInfo:notification.request.content.userInfo];
-  if([[notification.request.content.userInfo valueForKey:@"show_in_foreground"] isEqual:@YES]) {
+  if([[notification.request.content.userInfo valueForKey:@"show_in_foreground"] isEqual:@YES]){
     completionHandler(UNNotificationPresentationOptionAlert | UNNotificationPresentationOptionBadge | UNNotificationPresentationOptionSound);
-  } else {
+  }else{
     completionHandler(UNNotificationPresentationOptionNone);
   }
 }
 
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)())completionHandler {
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)())completionHandler
+{
   NSDictionary* userInfo = [[NSMutableDictionary alloc] initWithDictionary: response.notification.request.content.userInfo];
   [userInfo setValue:@YES forKey:@"opened_from_tray"];
   [[NSNotificationCenter defaultCenter] postNotificationName:FCMNotificationReceived object:self userInfo:userInfo];
 }
-#else
-//You can skip this method if you don't want to use local notification
-- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
-  [[NSNotificationCenter defaultCenter] postNotificationName:FCMNotificationReceived object:self + userInfo:notification.userInfo];
+
+-(void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
+  [[NSNotificationCenter defaultCenter] postNotificationName:FCMNotificationReceived object:self userInfo:notification.userInfo];
 }
-#endif
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler{
   [[NSNotificationCenter defaultCenter] postNotificationName:FCMNotificationReceived object:self userInfo:userInfo];
