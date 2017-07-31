9cc07ee2625f6715c1a96dce442ad55f1d698aea
Libin Lu <llu@evolenthealth.com>
2017-07-31 10:18:43 -0400

    Update MessagingService.java

--- android/src/main/java/com/evollu/react/fcm/MessagingService.java
+++ android/src/main/java/com/evollu/react/fcm/MessagingService.java
@@ -22,7 +22,33 @@ public class MessagingService extends FirebaseMessagingService {
         i.putExtra("data", remoteMessage);
         handleBadge(remoteMessage);
         buildLocalNotification(remoteMessage);
-        sendOrderedBroadcast(i, null);
+        
+        // We need to run this on the main thread, as the React code assumes that is true.
+        // Namely, DevServerHelper constructs a Handler() without a Looper, which triggers:
+        // "Can't create handler inside thread that has not called Looper.prepare()"
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            public void run() {
+                // Construct and load our normal React JS code bundle
+                ReactInstanceManager mReactInstanceManager = ((ReactApplication) getApplication()).getReactNativeHost().getReactInstanceManager();
+                ReactContext context = mReactInstanceManager.getCurrentReactContext();
+                // If it's constructed, send a notification
+                if (context != null) {
+                    context.sendOrderedBroadcast(message, null);
+                } else {
+                    // Otherwise wait for construction, then send the notification
+                    mReactInstanceManager.addReactInstanceEventListener(new ReactInstanceManager.ReactInstanceEventListener() {
+                        public void onReactContextInitialized(ReactContext context) {
+                            context.sendOrderedBroadcast(message, null);
+                        }
+                    });
+                    if (!mReactInstanceManager.hasStartedCreatingInitialContext()) {
+                        // Construct it in the background
+                        mReactInstanceManager.createReactContextInBackground();
+                    }
+                }
+            }
+        });
     }
 
     public void handleBadge(RemoteMessage remoteMessage) {
