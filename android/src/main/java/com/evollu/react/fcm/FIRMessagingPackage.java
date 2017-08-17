503dd7a7efa017851a55a644c231bba6654922f1
Will <will@laserlike.com>
2017-08-17 16:02:10 -0700

    compatible with RN0.46

--- android/src/main/java/com/evollu/react/fcm/FIRMessagingPackage.java
+++ android/src/main/java/com/evollu/react/fcm/FIRMessagingPackage.java
@@ -24,6 +24,11 @@ public class FIRMessagingPackage implements ReactPackage {
     }
 
     @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+ 
+    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList();
     }
