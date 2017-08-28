2c172061cf684f5a0c824bd1bc996db909580da3
Libin Lu <llu@evolenthealth.com>
2017-08-28 10:14:38 -0400

    Update FIRMessagingPackage.java

--- android/src/main/java/com/evollu/react/fcm/FIRMessagingPackage.java
+++ android/src/main/java/com/evollu/react/fcm/FIRMessagingPackage.java
@@ -22,6 +22,11 @@ public class FIRMessagingPackage implements ReactPackage {
         modules.add(new FIRMessagingModule(reactContext));
         return modules;
     }
+    
+    // support rn < 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
