0cce449d00a07b8d0f7d9c65052d97e091b9597e
Nastasia <muaaaaaaa@mail.ru>
2018-02-07 12:43:57 +0300

    add deleteToken and getEntityToken methods for android

--- index.js
+++ index.js
@@ -68,6 +68,14 @@ FCM.getFCMToken = () => {
   return RNFIRMessaging.getFCMToken();
 };
 
+FCM.getEntityFCMToken = () => {
+  return RNFIRMessaging.getEntityFCMToken();
+}
+
+FCM.deleteFCMToken = () => {
+  return RNFIRMessaging.deleteFCMToken();
+}
+
 FCM.deleteInstanceId = () =>{
   return RNFIRMessaging.deleteInstanceId();
 };
