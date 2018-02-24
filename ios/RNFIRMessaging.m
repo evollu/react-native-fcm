ef7902954334c0d7409e9f53bbd2ae15ad3ef0d4
Libin Lu <llu@evolenthealth.com>
2018-02-24 12:53:10 -0500

    Update RNFIRMessaging.m

--- ios/RNFIRMessaging.m
+++ ios/RNFIRMessaging.m
@@ -322,7 +322,7 @@ RCT_EXPORT_MODULE();
         [[FIRMessaging messaging] setDelegate:self];
     });
   
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
     if(!jsHandlerRegistered){
       [self sendPendingNotifications];
     }
