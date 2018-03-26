2dccdbee65ab9708a7a178d28f5d3d79bd0bf930
Libin Lu <llu@evolenthealth.com>
2018-03-26 10:44:42 -0400

    Update App.js

--- Examples/simple-fcm-client/app/App.js
+++ Examples/simple-fcm-client/app/App.js
@@ -63,6 +63,10 @@ class MainPage extends Component {
         console.log("APNS TOKEN (getFCMToken)", token);
       });
     }
+	  
+	  // topic example
+	  // FCM.subscribeToTopic('sometopic')
+	  // FCM.unsubscribeFromTopic('sometopic')
   }
 
   showLocalNotification() {
